c8db4d3a40c70d2410ca85cad23be3e1a63f3b5e
Prabodh <3585811+prabodh1194@users.noreply.github.com>
2025-11-19 07:14:09 +0530

    organise code a little bit

---
+++ tests/value_test.py
@@ -0,0 +1,36 @@
+from micrograd import Value, draw_root
+
+x1 = Value(2.0, label="x1")
+x2 = Value(0.0, label="x2")
+
+w1 = Value(-3.0, label="w1")
+w2 = Value(1.0, label="w2")
+
+# 6
+# 8
+# 6.8813735870195432
+b = Value(6.8813735870195432, label="b")
+
+x1w1 = x1 * w1
+x1w1.label = "x1*w1"
+x2w2 = x2 * w2
+x2w2.label = "x2*w2"
+
+x1w1x2w2 = x1w1 + x2w2
+x1w1x2w2.label = "x1*w1 + x2*w2"
+
+n = x1w1x2w2 + b
+n.label = "n"
+
+o = n.tanh()
+"""
+# can also be expressed as:
+
+e = (2 * n).exp()
+o = (e - 1) / (e + 1)
+"""
+
+o.label = "o"
+o.backward()
+
+draw_root(o, render=True)
