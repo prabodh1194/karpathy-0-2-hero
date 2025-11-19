8321a02fd385f26957d77971542008ef93d84515
Prabodh <3585811+prabodh1194@users.noreply.github.com>
2025-11-19 06:51:43 +0530

    decompose backward pass

--- micrograd.py
+++ micrograd.py
@@ -82,26 +82,55 @@ class Value:
         return f"Value(data={self.data}, label={self.label})"
 
     def __add__(self, other: Self) -> Value:
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), "+")
 
         def _backward() -> None:
-            self.grad = 1.0 * out.grad
-            other.grad = 1.0 * out.grad
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
 
         out._backward = _backward
 
         return out
 
-    def __mul__(self, other: Self) -> Value:
+    def __radd__(self, other: Self) -> Value:
+        return self + other
+
+    def __mul__(self, _other: Self | int | float) -> Value:
+        other = _other if isinstance(_other, Value) else Value(_other)
         out = Value(self.data * other.data, (self, other), "*")
 
         def _backward() -> None:
-            self.grad = out.grad * other.data
-            other.grad = out.grad * self.data
+            self.grad += out.grad * other.data
+            other.grad += out.grad * self.data
 
         out._backward = _backward
 
         return out
+
+    def __rmul__(self, other: Self) -> Value:
+        return self * other
+
+    def __truediv__(self, other: Self) -> Value:
+        return self * other**-1
+
+    def __pow__(self, other: int | float) -> Value:
+        assert isinstance(other, (int, float)), "power must be raised to a int or float"
+        out = Value(self.data**other, (self,), f"**{other}")
+
+        def _backward() -> None:
+            n = other
+            self.grad += n * self.data ** (n - 1) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __neg__(self) -> Value:
+        return self * -1
+
+    def __sub__(self, other: Value) -> Value:
+        return self + (-other)
 
     def tanh(self) -> Value:
         x = self.data
@@ -109,7 +138,18 @@ class Value:
         out = Value(t, (self,), "tanh")
 
         def _backward() -> None:
-            self.grad = (1 - t**2) * out.grad
+            self.grad += (1 - t**2) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def exp(self) -> Value:
+        x = self.data
+        out = Value(math.exp(x), (self,), "exp")
+
+        def _backward() -> None:
+            self.grad = out.data * out.grad
 
         out._backward = _backward
 
